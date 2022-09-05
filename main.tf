55b4e5871f597ae158b69f2d781ecfeaba2bdc1f
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-09-05 12:55:05 +0300

    generic elastic password

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 # Providers
 provider "aws" {
   region = var.region
-  secret_key = var.secret_key 
-  access_key = var.access_key
 }
 
 resource "aws_instance" "master" {
@@ -17,12 +15,16 @@ provisioner "remote-exec" {
       #!/bin/bash
       "sudo apt update -y && sudo apt install docker.io docker-compose -y",
       "wget https://gist.github.com/moodyomar/b95f9cc09d90581f19caa9295b874b53/raw/607573858498829b7489a53d594b91391047717e/.bash_aliases && source ~/.bash_aliases && source ~/.bashrc",
-      "alias docker='sudo docker'",
       "sudo chown $USER /var/run/docker.sock",
-      "git clone https://github.com/deviantony/docker-elk.git",
-      "cd docker-elk && docker-compose up -d",
-      "docker-compose exec elasticsearch bin/elasticsearch-setup-passwords auto > passwords.txt",
-      "docker ps",
+      "git clone https://github.com/moodyomar/docker-elk.git",
+      "cd docker-elk",
+      "echo ELASTIC_VERSION=8.4.1 > .env",
+      "echo ELASTIC_PASSWORD=${var.ELK_PASSWORD} >> .env ",
+      "echo LOGSTASH_INTERNAL_PASSWORD=${var.ELK_PASSWORD} >> .env ",
+      "echo KIBANA_SYSTEM_PASSWORD=${var.ELK_PASSWORD} >> .env ",
+      "sudo docker-compose up -d",
+      "sudo docker ps && sleep 5",
+      # "sudo docker exec docker-elk_elasticsearch_1 bash -c 'yes | ./bin/elasticsearch-reset-password -u elastic -a > passwords.txt; cat passwords.txt ; ls'",
     ]
     connection {
       type        = "ssh"
@@ -33,7 +35,7 @@ provisioner "remote-exec" {
   }
 
    provisioner "local-exec" {
-    command = "sleep 3 && open http://${self.public_ip}:5601 || xdg-open http://${self.public_ip}:5601"
+    command = "echo lunching Kibana Dashboard... && sleep 45 && open http://${self.public_ip}:5601 || xdg-open http://${self.public_ip}:5601"
   } 
 
   tags = {
@@ -50,7 +52,8 @@ vpc_security_group_ids = [var.security_group]
 provisioner "remote-exec" {
     inline = [
       #!/bin/bash
-      "sudo apt update -y",
+      # "sudo apt update -y",
+      "uname -v",
     ]
     connection {
       type        = "ssh"
